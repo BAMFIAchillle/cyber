44949134416dbabbd15e574eaded7d455297e420
BAMFIAchillle <akbamfi@gmail.com>
2024-10-17 10:22:40 +0200

    fixed_api

--- public/js/app.js
+++ public/js/app.js
@@ -1,124 +1,98 @@
-let auth0 = null;
-
+let auth0Client = null;
 
 const fetchAuthConfig = () => fetch("/auth_config.json");
-
 
 const configureClient = async () => {
   const response = await fetchAuthConfig();
   const config = await response.json();
-
-
-  auth0 = await createAuth0Client({
+  auth0Client = await auth0.createAuth0Client({
     domain: config.domain,
-    client_id: config.clientId
+    clientId: config.clientId,
+    authorizationParams: {
+      audience: config.audience, // NEW - add the audience value
+    },
   });
 };
-
 
 window.onload = async () => {
   await configureClient();
 
-
-  // NEW - update the UI state
   updateUI();
 
-
-  const isAuthenticated = await auth0.isAuthenticated();
-
+  const isAuthenticated = await auth0Client.isAuthenticated();
 
   if (isAuthenticated) {
     // show the gated content
     return;
   }
 
-
   // NEW - check for the code and state parameters
   const query = window.location.search;
   if (query.includes("code=") && query.includes("state=")) {
-	console.log("OAuth2 state: " + new URLSearchParams(window.location.search).get("state"));
-	console.log("OAuth2 authorization code: " + new URLSearchParams(window.location.search).get("code"));
-
-
     // Process the login state
-    await auth0.handleRedirectCallback();
-    
-    updateUI();
+    await auth0Client.handleRedirectCallback();
 
+    updateUI();
 
     // Use replaceState to redirect the user away and remove the querystring parameters
     window.history.replaceState({}, document.title, "/");
   }
 };
 
-
-// NEW
 const updateUI = async () => {
-  const isAuthenticated = await auth0.isAuthenticated();
-
-
-  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  const isAuthenticated = await auth0Client.isAuthenticated();
   document.getElementById("btn-login").disabled = isAuthenticated;
-
+  document.getElementById("btn-logout").disabled = !isAuthenticated;
+  document.getElementById("btn-call-api").disabled = !isAuthenticated;
 
   if (isAuthenticated) {
     document.getElementById("gated-content").classList.remove("hidden");
 
+    document.getElementById("ipt-access-token").innerHTML =
+      await auth0Client.getTokenSilently();
 
-    // Get the access token and user profile
-    document.getElementById("ipt-access-token").innerHTML = await auth0.getTokenSilently();
-    document.getElementById("ipt-user-profile").textContent = JSON.stringify(await auth0.getUser());
+    document.getElementById("ipt-user-profile").textContent = JSON.stringify(
+      await auth0Client.getUser()
+    );
   } else {
     document.getElementById("gated-content").classList.add("hidden");
   }
 };
 
-
 const login = async () => {
-  await auth0.loginWithRedirect({
-    redirect_uri: window.location.origin
+  await auth0Client.loginWithRedirect({
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    },
   });
 };
-
 
 const logout = () => {
-  auth0.logout({
-    returnTo: window.location.origin
+  auth0Client.logout({
+    logoutParams: {
+      returnTo: window.location.origin,
+    },
   });
 };
 
-/* 
-const callApi = async () => {
-  try {
-
-
-    // Get the access token from the Auth0 client
-    const token = await auth0.getTokenSilently();
-
-
-    // Make the call to the API, setting the token
-    // in the Authorization header
-    const response = await fetch("/api/external", {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-
-
-    // Fetch the JSON result
-    const responseData = await response.json();
-
-
-    // Display the result in the output element
-    const responseElement = document.getElementById("api-call-result");
-
-
-    responseElement.innerText = JSON.stringify(responseData, {}, 2);
-
-
-} catch (e) {
-    // Display errors in the console
-    console.error(e);
-  }
-};
- */
+const callApi = async () => { 
+  try { 
+  // Get the access token from the Auth0 client 
+  const token = await auth0Client.getTokenSilently(); 
+  // Make the call to the API, setting the token 
+  // in the Authorization header 
+  const response = await fetch("/api/external", { 
+  headers: { 
+   Authorization: `Bearer ${token}` 
+  } 
+  }); 
+  // Fetch the JSON result 
+  const responseData = await response.json(); 
+  // Display the result in the output element 
+  const responseElement = document.getElementById("api-call-result"); 
+  responseElement.innerText = JSON.stringify(responseData, {}, 2); 
+  } catch (e) { 
+  // Display errors in the console 
+  console.error(e); 
+  } 
+  };
